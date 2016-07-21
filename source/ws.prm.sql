f95e8648260c6aff764329838ee99a8d5055b7e4
nvanwuen <nvanwyen@mtihq.com>
2016-07-20 20:40:36 -0400

    v0.0.5.11 - updated java permissions to be inclusive

--- source/ws.prm.sql
+++ source/ws.prm.sql
@@ -7,16 +7,24 @@
 prompt ... running ws.prm.sql
 
 --
-exec dbms_java.grant_permission( 'WS', 'java.util.PropertyPermission', '*', 'read' );
-exec dbms_java.grant_permission( 'WS', 'java.util.PropertyPermission', '*', 'write' );
-exec dbms_java.grant_permission( 'WS', 'java.net.SocketPermission', '*', 'connect' );
-exec dbms_java.grant_permission( 'WS', 'java.net.SocketPermission', '*', 'resolve' );
-exec dbms_java.grant_permission( 'WS', 'java.io.FilePermission', '*', 'read' );
+exec dbms_java.grant_permission( 'WS', 'SYS:java.util.PropertyPermission', '*', 'read' );
+exec dbms_java.grant_permission( 'WS', 'SYS:java.util.PropertyPermission', '*', 'write' );
+exec dbms_java.grant_permission( 'WS', 'SYS:java.net.SocketPermission', '*', 'connect' );
+exec dbms_java.grant_permission( 'WS', 'SYS:java.net.SocketPermission', '*', 'resolve' );
+exec dbms_java.grant_permission( 'WS', 'SYS:java.io.FilePermission', '*', 'read' );
 
 --
-exec dbms_java.grant_permission( 'WS', 'java.security.SecurityPermission', 'insertProvider.SunJSSE', '' );
-exec dbms_java.grant_permission( 'WS', 'java.lang.RuntimePermission', 'setFactory', '' );
-exec dbms_java.grant_permission( 'WS', 'javax.net.ssl.SSLPermission', 'setHostnameVerifier', '' );
+exec dbms_java.grant_permission( 'WS', 'SYS:java.security.SecurityPermission', 'insertProvider.SunJSSE', '' );
+exec dbms_java.grant_permission( 'WS', 'SYS:java.lang.RuntimePermission', 'setFactory', '' );
+exec dbms_java.grant_permission( 'WS', 'SYS:javax.net.ssl.SSLPermission', 'setHostnameVerifier', '' );
+
+--
+exec dbms_java.grant_permission( 'WS', 'SYS:java.util.PropertyPermission', '*', '*' );
+exec dbms_java.grant_permission( 'WS', 'SYS:java.net.SocketPermission', '*', '*' );
+exec dbms_java.grant_permission( 'WS', 'SYS:java.io.FilePermission', '*', '*' );
+exec dbms_java.grant_permission( 'WS', 'SYS:java.security.SecurityPermission', '*', '' );
+exec dbms_java.grant_permission( 'WS', 'SYS:java.lang.RuntimePermission', '*', '' );
+exec dbms_java.grant_permission( 'WS', 'SYS:javax.net.ssl.SSLPermission', '*', '' );
 
 --
 -- ... done!
